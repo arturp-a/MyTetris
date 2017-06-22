4213c227c19d82a4e3bcd7ef26a4dbcbb501dc1d
ArturPetrosyan3 <h119artur@gmail.com>
2017-06-22 20:44:59 +0400

    shape is commented

--- board.h
+++ board.h
@@ -2,12 +2,16 @@
 #define BOARD_H
 
 #include <QWidget>
-
+#include "shape.h"
 class Board  : public QWidget
 {
 public:
     Board();
     Board(QWidget *parent = 0);
+private:
+    enum { BoardWidth = 10, BoardHeight = 22 };
+    //Shape shape[]
+    
 };
 
 #endif // BOARD_H
